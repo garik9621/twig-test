b755610b4117947558d634d9e5bdf05901533749
Igor Seliverstov <seliverstov@logema.org>
2020-08-25 16:06:06 +0400

    Подключил бутстрап Вывел ссылки верхнего меню из json

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,24 @@ const twig = require('gulp-twig')
 const browserSync = require('browser-sync').create()
 const del = require('del')
 const sass = require('gulp-sass')
+//const babel = require('gulp-babel')
+
+const twigData = {
+	nav: [
+		{link: '#1', name: 'Ссылка 1'},
+		{link: '#2', name: 'Ссылка 2'},
+		{link: '#3', name: 'Ссылка 3'},
+		{link: '#4', name: 'Ссылка 4'},
+	],
+	posts: [
+		{
+			title: 'Заголовок блока',
+			link: '#link1',
+			img: '',
+			text: 'зимут прекрасно решает радиант. Вселенная достаточно огромна, чтобы Южный Треугольник ничтожно перечеркивает астероидный Юпитер. У планет-гигантов нет твёрдой поверхности, таким образом узел вызывает сарос, в таком случае эксцентриситеты и наклоны орбит возрастают. '
+		}
+	]
+}
 
 const sourcePaths = {
 	pages: 'source/html/*.html',
@@ -20,7 +38,10 @@ const buildPaths = {
 
 const buildTwigPage = function() {
 	return src(sourcePaths.twig)
-		.pipe(twig())
+		.pipe(twig({
+			data: twigData,
+			useFileContents: true
+		}))
 		.pipe(dest(buildPaths.pages))
 }
 
@@ -36,9 +57,14 @@ const buildStyles = function() {
 		.pipe(browserSync.stream())
 }
 
+const buildJs = function() {
+
+}
+
 const watcher = function(done){
 	watch(['source/html/twig/**/*.twig'], series(buildTwigPage, reload))
 	watch(['source/html/*.html'], series(buildPage, reload))
+	watch(['source/scss/*.scss'], buildStyles)
 	done()
 }
 
@@ -49,7 +75,7 @@ const clean = function() {
 function runServer (done) {
 	browserSync.init({
 		server: {
-			baseDir: ['./build/pages/'],
+			baseDir: ['./', './build/pages/'],
 			logLevel: 'debug',
 		},
 		ghostMode: false,
